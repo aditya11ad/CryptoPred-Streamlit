cea0921d8990c30f8a80893c1a8b63a628babd6a
Aditya Nagdiya <aditya22.ad@gmail.com>
2021-05-22 15:51:54 +0530

    Update Bitcoin-yfinance.py

--- Bitcoin-yfinance.py
+++ Bitcoin-yfinance.py
@@ -26,7 +26,7 @@ col3.line_chart(tickerDf.Close)
 #######################################
 import matplotlib.pyplot as plt
     
-if st.button('Show Prediction'):
+if st.button('Show Next 10 days Prediction'):
     df=tickerDf[['Close']]    
 
     from tensorflow import keras
